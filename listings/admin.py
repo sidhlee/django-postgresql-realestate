6b683d1b05d04a2a6c404cee5c4ecfb884c43318
Sid Hayoun Lee <sidhlee@gmail.com>
2021-08-09 07:27:33 -0400

    add columns to the listings table

--- listings/admin.py
+++ listings/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Listing
 
 # Register your models here.
-admin.site.register(Listing)
+
+class ListingAdmin (admin.ModelAdmin):
+  list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+
+admin.site.register(Listing, ListingAdmin)
