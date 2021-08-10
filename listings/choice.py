fe57fbdf183ffab7432773df11be5ea7b3644b99
Sid Hayoun Lee <sidhlee@gmail.com>
2021-08-10 19:03:35 -0400

    create search options dictionaries

---
+++ listings/choice.py
@@ -0,0 +1,40 @@
+bedroom_choices = {
+  '1': 1,
+  '2': 2,
+  '3': 3,
+  '4': 4,
+  '5': 5,
+  '6': 6,
+  '7': 7,
+  '8': 8,
+  '9': 9,
+  '10': 10,  
+}
+
+price_choices = {  
+  '600000': '$600,000',
+  '700000': '$700,000',
+  '800000': '$800,000',
+  '900000': '$900,000',
+  '1000000': '$1M',
+  '1100000': '$1.1M',
+  '1200000': '$1.2M',
+  '1300000': '$1.3M',
+  '1400000': '$1.4M',
+  '1500000': '$1.5M+',
+}
+
+province_choices = {
+  'NL': 'Newfoundland and Labrador',
+  'PE': 'Prince Edward Island',
+  'NS': 'Nova Scotia',
+  'NB': 'New Brunswick',
+  'QC': 'Quebec',
+  'ON': 'Ontario',
+  'MB': 'Manitoba',
+  'SK': 'Saskatchewan',
+  'BC': 'British Columbia',
+  'YT': 'Yukon',
+  'NT': 'Northwest Territories',
+  'NU': 'Nunavut',
+} 
