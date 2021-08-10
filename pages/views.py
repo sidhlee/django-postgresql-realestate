6f6f480a7880c5bfe3ab0f783903421085662d47
Sid Hayoun Lee <sidhlee@gmail.com>
2021-08-10 17:13:46 -0400

    fetch realtors and mvp to render to the about template

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from listings.models import Listing
+from realtors.models import Realtor
 
 # Create your views here.
 
@@ -16,4 +17,12 @@ def index(request):
   return render(request, 'pages/index.html', context)
 
 def about(request):
-  return render(request, 'pages/about.html')
+  realtors = Realtor.objects.order_by('-hired_date')
+  mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+  context = {
+    "realtors": realtors,
+    "mvp_realtors": mvp_realtors
+  }
+
+  return render(request, 'pages/about.html', context)
--- realtors/admin.py
+++ realtors/admin.py
@@ -3,10 +3,11 @@ from .models import Realtor
 
 
 class RealtorAdmin(admin.ModelAdmin):
-  list_display = ('id', 'name', 'email', 'hired_date')
+  list_display = ('id', 'name', 'email', 'hired_date', 'is_mvp')
   list_display_links = ('id', 'name')
   search_fields = ('name',)
   list_per_page = 25
+  list_editable = ('is_mvp',)
 
 # Register your models here.
 admin.site.register(Realtor, RealtorAdmin)
