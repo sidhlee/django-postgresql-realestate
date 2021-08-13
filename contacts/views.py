6c4830e90f1618313f1543a9a13bdad2b47b02ac
Sid Hayoun Lee <sidhlee@gmail.com>
2021-08-13 17:47:50 -0400

    prevent logged-in user from submittin inquiry for the same listing

--- contacts/views.py
+++ contacts/views.py
@@ -13,6 +13,15 @@ def contact(request):
     email = request.POST['email']
     phone = request.POST['phone']
     message = request.POST['message']
+
+    # Check if the logged-in user has made the inquiry already
+    if request.user.is_authenticated:
+      user_id = request.user.id
+      has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+      if has_contacted:
+        messages.error(request, 'you have already made an inquiry for this listing')
+        return redirect('/listings/' + listing_id)
+
   
     contact = Contact(
       listing=listing,
